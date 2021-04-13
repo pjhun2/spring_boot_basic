fb1017d61bc7d9ce6fb7e777f2b7151e97c6d3f2
pjhun2 <netwo115@naver.com>
2021-04-13 19:38:07 +0900

    Spring boot day 1

--- src/main/java/com/spring/bo/BoApplication.java
+++ src/main/java/com/spring/bo/BoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication // enableAutoConfiguration, ComponentScan, Cofniguration을 하나로 묶은 어노테이션
                        // - 스프링부트를 기동하기 위해서는 main 메소드가 필요한데 여기에SpringApplication.run(BoApplication.class, args); 이 부분이 들어 간다.
-                       // 해당 어노테이션을 설정한 클래스가 있는 패키지를 최상위 패키지로 인식하고 컴포넌트 스캔을 수행하기 때문에 해당 어노테이션이 있는 클래스의 파일 위치 또한 중요하다
+                       // 해당 어노테이션을 설정한 클래스가 있는 패키지를 최상위 패키지로 인식하고 컴포넌트 스캔을 수행하기 때문에 해당 어노테이션이 있는 클래스의 파일 위치 또한 중요하
 
 public class BoApplication {
 
@@ -14,3 +14,9 @@ public class BoApplication {
 	}
 
 }
+
+
+
+// 정적 리소스 Spring boot 프로젝트는 별도의 커스터마이징이 없는 경우 정적 리소스 위치는
+// Static, Public , Resources , META-INF/resources
+// META-INF/resources/test/test.txt 경로에 파일이 위치할 경우 /test/test.txt이다.
